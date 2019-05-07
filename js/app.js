92a830e2ad97d2f2345ed419fe0052329d145f49
surfwalker <alexanderkreid@gmail.com>
2019-05-06 19:43:53 -0700

    Added .render method to alki object literal for testing.

--- js/app.js
+++ js/app.js
@@ -8,96 +8,138 @@ var shopList = document.getElementById('shopList');
 var pike = {
   name: 'Pike',
   location: '1st and Pike',
+  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
   minCustomerPerHr: 23,
   maxCustomerPerHr: 65,
   avgCookiesPerCustomer: 6.3,
-  cookiesPurchasedPerHr: [],
+  recordOfSalesPerHour: [],
   randomCustomersPerHr: function(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min; 
   },
-  cookiesPurchasedPerHr: function(avgPerCustomer, customersPerHr) {
-    return Math.ceil(avgPerCustomer * customersPerHr); 
-  },
-  render: function() {
-    // create an element
-    var liEl = document.createElement('li');
-    console.log('liEl is', liEl);
-    // give the element content
-    liEl.textContent = this.nickname;
-    console.log('liEl with content', liEl);
-    // append the element to the dom
-    catlist.appendChild(liEl);
+  cookiesPurchasedPerHr: function(customersPerHr, avgPerCustomer) {
+    this.recordOfSalesPerHour = [];
+    for(var i = 0; i <= 14; i++) {
+      this.recordOfSalesPerHour.push(Math.ceil(this.randomCustomersPerHr(this.minCustomerPerHr, this.maxCustomerPerHr), this.avgPerCustomer));
+    }
   }
 };
 
-var trillian = {
-  name: 'Trill',
-  nickname: 'Fuzzbucket',
-  age: 10,
-  colors: ['black', 'brown', 'orange'],
-  isAMeower: true,
-  introduction: function(){
-    console.log('The call me ' + this.nickname + ' and I am ' + this.age + ' years old.')
+// SeaTac location object literal
+var seaTac = {
+  name: 'SeaTac',
+  location: 'SeaTac Airport',
+  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  minCustomerPerHr: 3,
+  maxCustomerPerHr: 24,
+  avgCookiesPerCustomer: 1.2,
+  recordOfSalesPerHour: [],
+  randomCustomersPerHr: function(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
   },
-  render: function() {
-    // create an element
-    var liEl = document.createElement('li');
-    console.log('liEl is', liEl);
-    // give the element content
-    liEl.textContent = this.nickname;
-    console.log('liEl with content', liEl);
-    // append the element to the dom
-    catlist.appendChild(liEl);
+  cookiesPurchasedPerHr: function(customersPerHr, avgPerCustomer) {
+    this.recordOfSalesPerHour = [];
+    for(var i = 0; i <= 14; i++) {
+      this.recordOfSalesPerHour.push(Math.ceil(this.randomCustomersPerHr(this.minCustomerPerHr, this.maxCustomerPerHr), this.avgPerCustomer));
+    }
   }
 };
 
-var aloysius = {
-  name: 'Aloysius',
-  nickname: 'Alley Cat',
-  age: 1,
-  colors: ['gray', 'black', 'brown', 'white'],
-  isAMeower: false,
-  introduction: function(){
-    console.log('The call me ' + this.nickname + ' and I am ' + this.age + ' years old.')
+// Seattle Center location object literal
+var seattle = {
+  name: 'Seattle',
+  location: 'Seattle Center',
+  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  minCustomerPerHr: 11,
+  maxCustomerPerHr: 38,
+  avgCookiesPerCustomer: 3.7,
+  recordOfSalesPerHour: [],
+  randomCustomersPerHr: function(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
   },
-  render: function() {
-    // create an element
-    var liEl = document.createElement('li');
-    console.log('liEl is', liEl);
-    // give the element content
-    liEl.textContent = this.nickname;
-    console.log('liEl with content', liEl);
-    // append the element to the dom
-    catlist.appendChild(liEl);
+  cookiesPurchasedPerHr: function(customersPerHr, avgPerCustomer) {
+    this.recordOfSalesPerHour = [];
+    for(var i = 0; i <= 14; i++) {
+      this.recordOfSalesPerHour.push(Math.ceil(this.randomCustomersPerHr(this.minCustomerPerHr, this.maxCustomerPerHr), this.avgPerCustomer));
+    }
   }
 };
 
-var agador = {
-  name: 'Agador Spartacus',
-  nickname: 'Ag',
-  age: 1,
-  colors: ['gray', 'black', 'brown', 'white'],
-  isAMeower: true,
-  introduction: function(){
-    console.log('The call me ' + this.nickname + ' and I am ' + this.age + ' years old.')
+// Capitol Hill location object literal
+var capHill = {
+  name: 'Capitol Hill',
+  location: 'Capitol Hill',
+  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  minCustomerPerHr: 20,
+  maxCustomerPerHr: 38,
+  avgCookiesPerCustomer: 2.3,
+  recordOfSalesPerHour: [],
+  randomCustomersPerHr: function(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
   },
-  render: function() {
-    // create an element
-    var liEl = document.createElement('li');
-    console.log('liEl is', liEl);
-    // give the element content
-    liEl.textContent = this.nickname;
-    console.log('liEl with content', liEl);
-    // append the element to the dom
-    catlist.appendChild(liEl);
+  cookiesPurchasedPerHr: function(customersPerHr, avgPerCustomer) {
+    this.recordOfSalesPerHour = [];
+    for(var i = 0; i <= 14; i++) {
+      this.recordOfSalesPerHour.push(Math.ceil(this.randomCustomersPerHr(this.minCustomerPerHr, this.maxCustomerPerHr), this.avgPerCustomer));
+    }
   }
 };
 
-var cats = [buddyCat, trillian, aloysius, agador];
+// Alki location object literal
+var alki = {
+  name: 'Alki',
+  location: 'Alki',
+  hoursOfOperation: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
+  minCustomerPerHr: 2,
+  maxCustomerPerHr: 16,
+  avgCookiesPerCustomer: 4.6,
+  recordOfSalesPerHour: [],
+  randomCustomersPerHr: function(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
+  },
+  cookiesPurchasedPerHr: function(customersPerHr, avgPerCustomer) {
+    this.recordOfSalesPerHour = [];
+    for(var i = 0; i <= 14; i++) {
+      this.recordOfSalesPerHour.push(Math.ceil(this.randomCustomersPerHr(this.minCustomerPerHr, this.maxCustomerPerHr), this.avgPerCustomer));
+    }
+  },
+  render: function() {
+    this.cookiesPurchasedPerHr();
+    for (var i = 0; i < this.hoursOfOperation.length; i++) {
+      // create an element
+      var ulEl = document.createElement('ul');
+      console.log('ulEl is', ulEl);
+      // give the element content
+      ulEl.textContent = this.hoursOfOperation[i] + ': ' + this.recordOfSalesPerHour[i] + ' cookies';
+      console.log('ulEl with content', ulEl);
+      ulEl.textContent = this.hoursOfOperation[i] + ': ' + this.recordOfSalesPerHour[i] + ' cookies';
+      console.log('ulEl with content', ulEl);
+      // append the element to the dom
+      shopList.appendChild(ulEl);
+      if(i === this.hoursOfOperation.length - 1) {
+        var ulEl = document.createElement('ul');
+        ulEl.textContent = 'Total: ' + this.recordOfSalesPerHour[i] + ' cookies';
+        ulEl.textContent = this.hoursOfOperation[i] + ': ' + this.recordOfSalesPerHour[i] + ' cookies';
+        shopList.appendChild(ulEl);
+      }
+    }
+  }
+};
 
-for (var i = 0; i < cats.length; i++) {
-  // console.log(cats[i].nickname)
-  cats[i].render()
-}
+
+
+var shops = [pike, seaTac, seattle, capHill, alki];
+
+// for (var i = 0; i < shops.length; i++) {
+//   // console.log(cats[i].nickname)
+//   cats[i].render()
+// }
